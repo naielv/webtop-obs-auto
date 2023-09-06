3300fe7c28dd74f399ec8d518bb3feffec58a264
naielv <109038805+naielv@users.noreply.github.com>
2023-09-06 16:50:12 +0200

    Add files via upload

---
+++ c.py
@@ -0,0 +1,26 @@
+
+import requests
+import json
+import datetime
+import os
+def tts(msg):
+    cmd = f'spd-say -l es -y female2 "{msg}"'
+    os.system(cmd)
+def callback(d: dict):
+    if d.get("event") == "message":
+        title = d.get("title")
+        message = d.get("message")
+        tags = d.get("tags")
+        time =  d.get("time")
+        time2= str(datetime.datetime.fromtimestamp(time))
+        if title != "clear_notification":
+            tts(message)
+    print(d)
+def main(topic):
+    resp = requests.get(f"https://ntfy.sh/{topic}/json", stream=True)
+    for line in resp.iter_lines():
+        if line:
+            callback(json.loads(line))
+
+if __name__ == "__main__":
+    main(input("Topic, please!\nntfy.sh topic: \n> "))
---
+++ dockerfile
@@ -0,0 +1,31 @@
+FROM kasmweb/core-ubuntu-focal:1.14.0
+USER root
+
+ENV HOME /home/kasm-default-profile
+ENV STARTUPDIR /dockerstartup
+ENV INST_SCRIPTS $STARTUPDIR/install
+WORKDIR $HOME
+
+######### Customize Container Here ###########
+
+
+
+RUN sudo add-apt-repository -y ppa:obsproject/obs-studio
+RUN sudo apt update
+RUN sudo apt-get install obs-studio speech-dispatcher -y
+
+
+COPY ./obs.json $HOME/Desktop/OBSScenes.json
+COPY ./c.py $HOME/Desktop/c.py
+
+
+######### End Customizations ###########
+
+RUN chown 1000:0 $HOME
+RUN $STARTUPDIR/set_user_permission.sh $HOME
+
+ENV HOME /home/kasm-user
+WORKDIR $HOME
+RUN mkdir -p $HOME && chown -R 1000:0 $HOME
+
+USER 1000
